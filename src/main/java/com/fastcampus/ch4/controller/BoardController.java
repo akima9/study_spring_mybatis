0dca449076fcd056e551036bbffab4a167509c66
devGiYong <akima9a@gmail.com>
2023-07-08 15:42:54 +0900

    DB 설정 & 게시물 수정 Controller 구

--- src/main/java/com/fastcampus/ch4/controller/BoardController.java
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -1,8 +1,12 @@
 package com.fastcampus.ch4.controller;
 
-import com.fastcampus.ch4.domain.BoardDto;
-import com.fastcampus.ch4.domain.PageHandler;
-import com.fastcampus.ch4.service.BoardService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,17 +15,64 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.fastcampus.ch4.domain.BoardDto;
+import com.fastcampus.ch4.domain.PageHandler;
+import com.fastcampus.ch4.service.BoardService;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
     @Autowired
     BoardService boardService;
+    
+    @PostMapping("/modify")
+    public String modify(BoardDto boardDto, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
+    	String writer = (String) session.getAttribute("id");
+    	boardDto.setWriter(writer);
+    	try {
+    		m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+            
+			int rowCnt = boardService.modify(boardDto);
+			if (rowCnt != 1) throw new Exception("Modify failed");
+			
+			rattr.addFlashAttribute("msg", "MOD_OK");
+			
+			return "redirect:/board/list";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			m.addAttribute(boardDto);
+			m.addAttribute("msg", "MOD_ERR");
+			return "board";
+		}
+    }
+    
+    @PostMapping("/write")
+    public String write(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rattr) {
+    	String writer = (String) session.getAttribute("id");
+    	boardDto.setWriter(writer);
+    	try {
+			int rowCnt = boardService.write(boardDto);
+			if (rowCnt != 1) throw new Exception("Write failed");
+			
+			rattr.addFlashAttribute("msg", "WRT_OK");
+			
+			return "redirect:/board/list";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			m.addAttribute(boardDto);
+			m.addAttribute("msg", "WRT_ERR");
+			return "board";
+		}
+    }
+    
+    @GetMapping("/write")
+    public String write(Model m) {
+    	m.addAttribute("mode", "new");
+    	return "board"; // 읽기와 쓰기에 사용, 쓰기에 사용할 때는 mode=new
+    }
 
     @PostMapping("/remove")
     public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rattr) {
